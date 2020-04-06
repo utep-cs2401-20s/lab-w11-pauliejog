61f7ce0458a9623a83beafd6bf95bba1623b89be
pauliejog <Elakpjd7!>
2020-04-06 14:44:40 -0600

    added newSorting meethod

--- scr/src/newSorting.java
+++ scr/src/newSorting.java
@@ -1,6 +1,49 @@
 public class newSorting {
-    newSorting(int[] A, int size) {
-        
+    void newSorting(int[] A, int size) {
+        if(A.length <= size)
+            quickSort(A);
+        else {
+            int mid = A.length/2;
+            int[] LH = new int[mid];
+            int[] RH = new int[A.length - mid];
+            populate(A,LH);
+            populate(A,RH);
+            newSorting(RH,size);
+            newSorting(LH,size);
+            mergeSortedHalves(A,LH,RH);
+        }
+
+    }//end newSorting
+
+    void quickSort(int[] A) {
+
+    }//end quickSort
+
+    void mergeSortedHalves(int[] A, int[] LH, int[] RH) {
+        int i = 0;//index for LH
+        int j = 0;//index for RH
+        int index = 0;//index for A
+        while(index < A.length) {
+            if(LH[i] < RH[j]) {
+                if(i < LH.length)
+                    A[index] = LH[i];
+                index++;
+                i++;
+            }
+            else {
+                if(j < RH.length)
+                    A[index] = RH[j];
+                index++;
+                j++;
+
+            }
+
+        }
+    }//end mergeSortedHalves
+
+    void populate(int[] A, int[] half) {
+
     }
 
-}
+
+}//end class
