be7b2b30cd11386e10150fc7eff8611410f96bab
pauliejog <Elakpjd7!>
2020-04-08 15:15:56 -0600

    added quickSort method code and 2 testers

--- scr/src/newSorting.java
+++ scr/src/newSorting.java
@@ -1,44 +1,62 @@
+/*
+* Paulie Jo Gonzalez
+* Lab-w11
+*
+ */
 public class newSorting {
     public void newSorting(int[] A, int size) {
-        if(A.length <= size)
-            quickSort(A);
-        else {
-            int mid = A.length/2;
-            int[] LH = new int[mid];
-            int[] RH = new int[A.length - mid];
+        if(A.length <= size)//base case
+            quickSort(A);//call on quickSort
+        else {//recursive call
+            int mid = A.length/2;//split array in half
+            int[] LH = new int[mid];//left half of A
+            int[] RH = new int[A.length - mid];//right half of A
             populate(A,LH,RH);//populate LH and RH
-            newSorting(RH,size);
-            newSorting(LH,size);
-            mergeSortedHalves(A,LH,RH);
-        }
-
+            newSorting(RH,size);//recursive call for right half
+            newSorting(LH,size);//recursive call for left half
+            mergeSortedHalves(A,LH,RH);//merge sorted right and sorted left halves
+        }//end else
     }//end newSorting
 
-    private void quickSort(int[] A) {
-        if(A.length < 2)
-            return;
-        else {
-            int index = partition(A);
-            int[] left = new int[index];
-            int[] right = new int[A.length-index];
-            quickSort(left);
-            quickSort(right);
-
-        }
-
+    private void quickSort(int[] A) {//will call on second quickSort method
+        quickSort(A,0,A.length-1);//starts at 0 and ends at last element
     }//end quickSort
 
-    private int partition(int[] A, int l, int r) {
-        return 0;
+    private void quickSort(int[] A, int start, int end) {//recursive quickSort method
+        int index = partition(A,start,end);//
+        if(start < index - 1)//left half of partitioned array
+            quickSort(A,start,index-1);
+        if(end > index)//right half of partitioned array
+            quickSort(A,index,end);
+    }//end quickSort
+
+    private int partition(int[] A, int l, int r) {//will partition from left to right
+        int pivot = A[l];//pivot is first element
+
+        while(l <= r) {
+            while(A[l] < pivot)//loop to find elements < pivot
+                l++;//track the index for left side
+            while(A[r] > pivot)//loop to find elements > pivot
+                r--;//track the index for right side
+            if(l <= r) {//considers new indices just found
+                //swap the values
+                int temp = A[l];
+                A[l] = A[r];
+                A[r] = temp;
+                l++;
+                r--;
+            }
+        }
+        return l;//starting index of all values >= pivot
     }//end partition
 
     private void mergeSortedHalves(int[] A, int[] LH, int[] RH) {
         int i = 0;//index for LH
         int j = 0;//index for RH
         int index = 0;//index for A
-        while(index < A.length) {
-            if(i < LH.length) {
-                if(j < RH.length) {
+        while(index < A.length) {//length of A
+            if(i < LH.length) {//if i is in bounds
+                if(j < RH.length) {//if j is in bounds
                     if(LH[i] < RH[j]) {
                         A[index] = LH[i];
                         index++;
@@ -50,13 +68,13 @@ public class newSorting {
                         j++;
                     }
                 }
-                else {
+                else {//j is oob
                     A[index] = LH[i];
                     index++;
                     i++;
                 }
             }
-            else if(j < RH.length) {
+            else if(j < RH.length) {//i is oob
                 A[index] = RH[j];
                 index++;
                 j++;
@@ -65,11 +83,11 @@ public class newSorting {
     }//end mergeSortedHalves
 
     private void populate(int[] A, int[] LH, int[] RH) {
-        for(int i = 0; i < LH.length; i++)
+        for(int i = 0; i < LH.length; i++)//loop to fill LH
             LH[i] = A[i];
 
         int index = A.length/2;
-        for(int j = 0; j < RH.length; j++,index++)
+        for(int j = 0; j < RH.length; j++,index++)//loop to fill RH
             RH[j] = A[index];
     }//end populate
 
--- scr/src/newSortingTester.java
+++ scr/src/newSortingTester.java
@@ -6,11 +6,15 @@ import org.junit.jupiter.api.Test;
 public class newSortingTester {
     /*
      * newSorting() method:
-     *
+     * PASSED
      */
     @Test
     public void testNewSorting1() {
-
+        newSorting ns = new newSorting();
+        int[] sort = {3,2,7,5,1,9,4};
+        int[] exp = {1,2,3,4,5,7,9};
+        ns.newSorting(sort,3);
+        assertArrayEquals(exp,sort);
     }
     /*
      * newSorting() method:
@@ -18,7 +22,11 @@ public class newSortingTester {
      */
     @Test
     public void testNewSorting2() {
-
+        newSorting ns = new newSorting();
+        int[] sort = {1,2,3,4};
+        int[] exp = {1,2,3,4};
+        ns.newSorting(sort,2);
+        assertArrayEquals(exp,sort);
     }
     /*
      * newSorting() method:
