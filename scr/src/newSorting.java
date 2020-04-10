d38c9ea2c12de405ae0badd61eec57fb9ef895f3
pauliejog <Elakpjd7!>
2020-04-10 12:46:26 -0600

    added test cases possibly figured out quickSort

--- scr/src/newSorting.java
+++ scr/src/newSorting.java
@@ -1,12 +1,17 @@
 /*
 * Paulie Jo Gonzalez
 * Lab-w11
-*
+* newSorting problem:
  */
 public class newSorting {
+    /*
+    * newSorting: recursive method that calls on quickSort and mergeSortedHalves method to sort an array depending
+    * on a size parameter
+     */
     public void newSorting(int[] A, int size) {
-        if(A.length <= size)//base case
+        if(A.length <= size) {//base case
             quickSort(A);//call on quickSort
+        }
         else {//recursive call
             int mid = A.length/2;//split array in half
             int[] LH = new int[mid];//left half of A
@@ -17,39 +22,9 @@ public class newSorting {
             mergeSortedHalves(A,LH,RH);//merge sorted right and sorted left halves
         }//end else
     }//end newSorting
-
-    private void quickSort(int[] A) {//will call on second quickSort method
-        quickSort(A,0,A.length-1);//starts at 0 and ends at last element
-    }//end quickSort
-
-    private void quickSort(int[] A, int start, int end) {//recursive quickSort method
-        int index = partition(A,start,end);//
-        if(start < index - 1)//left half of partitioned array
-            quickSort(A,start,index-1);
-        if(end > index)//right half of partitioned array
-            quickSort(A,index,end);
-    }//end quickSort
-
-    private int partition(int[] A, int l, int r) {//will partition from left to right
-        int pivot = A[l];//pivot is first element
-
-        while(l <= r) {
-            while(A[l] < pivot)//loop to find elements < pivot
-                l++;//track the index for left side
-            while(A[r] > pivot)//loop to find elements > pivot
-                r--;//track the index for right side
-            if(l <= r) {//considers new indices just found
-                //swap the values
-                int temp = A[l];
-                A[l] = A[r];
-                A[r] = temp;
-                l++;
-                r--;
-            }
-        }
-        return l;//starting index of all values >= pivot
-    }//end partition
-
+    /*
+     * mergeSortedHalves: method to merge the sorted halves together back into the given array (A)
+     */
     private void mergeSortedHalves(int[] A, int[] LH, int[] RH) {
         int i = 0;//index for LH
         int j = 0;//index for RH
@@ -81,7 +56,53 @@ public class newSorting {
             }
         }
     }//end mergeSortedHalves
+    /*
+    * quickSort: first quickSort method that takes an array and calls on the second quickSort method
+     */
+    private void quickSort(int[] A) {//will call on second quickSort method
+        quickSort(A,0,A.length-1);//starts at 0 and ends at last element
+    }//end quickSort
+    /*
+    * quickSort: recursive method to sort an array
+     */
+    private void quickSort(int[] A, int start, int end) {//recursive quickSort method
+        int index = partition(A, start, end);
+        if(index-1 > start)
+            quickSort(A, start, index-1);
+        if(index+1 < end)
+            quickSort(A, index+1, end);
 
+    }//end quickSort
+    /*
+    * partition: helper method to partition the array during quickSort
+     */
+    private int partition(int[] A, int l, int r) {//will partition from left to right
+        int pivot = A[l];//pivot is first element
+        int less = l+1;
+        while(less <= r) {
+            while(less < A.length && A[less] < pivot)//loop to find elements < pivot
+                less++;//track the index for left side
+            while(A[r] > pivot)//loop to find elements > pivot
+                r--;//track the index for right side
+            if(less <= r) {//considers new indices just found
+                //swap the values
+                int temp = A[less];
+                A[less] = A[r];
+                A[r] = temp;
+                less++;
+                r--;
+            }
+        }
+        //once less > r swap the pivot value with A[r] and return r
+        int hold = A[r];
+        A[r] = A[l];
+        A[l] = hold;
+
+        return r;//new index of pivot
+    }//end partition
+    /*
+    * populate: method to fill the left half and right half arrays
+     */
     private void populate(int[] A, int[] LH, int[] RH) {
         for(int i = 0; i < LH.length; i++)//loop to fill LH
             LH[i] = A[i];
@@ -90,6 +111,5 @@ public class newSorting {
         for(int j = 0; j < RH.length; j++,index++)//loop to fill RH
             RH[j] = A[index];
     }//end populate
-
-
 }//end class
+
--- scr/src/newSortingTester.java
+++ scr/src/newSortingTester.java
@@ -1,3 +1,8 @@
+/*
+ * Paulie Jo Gonzalez
+ * Lab-w11
+ * newSorting tester:
+ */
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.concurrent.TimeUnit;
 
@@ -6,6 +11,7 @@ import org.junit.jupiter.api.Test;
 public class newSortingTester {
     /*
      * newSorting() method:
+     * This test case tests the basic functionality of the newSorting method under normal circumstances.
      * PASSED
      */
     @Test
@@ -13,12 +19,13 @@ public class newSortingTester {
         newSorting ns = new newSorting();
         int[] sort = {3,2,7,5,1,9,4};
         int[] exp = {1,2,3,4,5,7,9};
-        ns.newSorting(sort,3);
+        ns.newSorting(sort,7);
         assertArrayEquals(exp,sort);
     }
     /*
-     * newSorting() method:
-     *
+     * newSorting(sort,2) method:
+     * This test case aims to test what happens if the given array is already sorted.
+     * PASSED
      */
     @Test
     public void testNewSorting2() {
@@ -29,27 +36,59 @@ public class newSortingTester {
         assertArrayEquals(exp,sort);
     }
     /*
-     * newSorting() method:
-     *
+     * newSorting(sort,) method:
+     * This tests case aims to stress the newSorting method given a reversely sorted array.
+     * PASSED
      */
     @Test
     public void testNewSorting3() {
+        newSorting ns = new newSorting();
+        int[] sort = {5,4,3,2,1};
+        int[] exp = {1,2,3,4,5};
+        ns.newSorting(sort,3);
+        assertArrayEquals(exp,sort);
 
     }
     /*
      * newSorting() method:
-     *
+     * This test case is to check that the method works correctly if the size parameter is <= the size of the array.
+     * PASSED
      */
     @Test
     public void testNewSorting4() {
+        newSorting ns = new newSorting();
+        int[] sort = {6,3,8,0,2,5};
+        int[] exp = {0,2,3,5,6,8};
+        ns.newSorting(sort,7);//size >= sort.length
+        assertArrayEquals(exp,sort);
 
     }
     /*
      * newSorting() method:
-     *
+     * This test case aims to stress the method with a size parameter of 0.
+     * FAILED.
      */
     @Test
     public void testNewSorting5() {
+        newSorting ns = new newSorting();
+        int[] sort = {6,3,8,0,2,5};
+        int[] exp = {0,2,3,5,6,8};
+        ns.newSorting(sort,0);//zero will cause a StackOverflowError
+        assertArrayEquals(exp,sort);
+
+    }
+    /*
+     * newSorting() method:
+     * This test case aims to stress the method with an input array that contains duplicates.
+     * PASSED
+     */
+    @Test
+    public void testNewSorting6() {
+        newSorting ns = new newSorting();
+        int[] sort = {4,4,4,4,4,4,4,4,4,4,1};
+        int[] exp = {1,4,4,4,4,4,4,4,4,4,4};
+        ns.newSorting(sort,5);//size is about half of the array size
+        assertArrayEquals(exp,sort);
 
     }
 
